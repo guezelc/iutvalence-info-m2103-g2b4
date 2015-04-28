ef933f8487c7348fb46b966d47df4f71ba73b389
GUEZEL Clement <guezelc@PC-B101-05.iut-valence.fr>
2015-04-28 09:22:09 +0200

    Code review + TODOs

--- src/Grille.java
+++ src/Grille.java
@@ -1,48 +1,55 @@
-
+// TODO rename class
 /**
+ *  creer une grille de puissance 4
  * @author guezelc
- * creer une grille de puissance 4
+ *
  */
 public class Grille 
 {
+	// TODO rename field
+	// TODO fix comment
 	/**
 	 * Nombre de lignes
 	 */
 	public final static int NOMBRE_DE_lIGNES = 6; 
 	
+	// TODO rename field
+	// TODO fix comment
 	/**
 	 * Nombre de colonnes
 	 */
-	
 	public final static int NOMBRE_DE_COLONNES = 7;
 	
+	// TODO rename field
+	// TODO fix comment
 	/** 
 	 * grille de puissance 4
 	 */
+	private Case[][] cases;
 
-	private Case[][] grille;
-
+	// TODO rename field
+	// TODO fix comment
+	// TODO top of column is an array??
 	/**
-	 * le sommet d'une colonne 
+	 * les indices des sommets de chaque colonne 
 	 */
 	private int[] sommet;
 	
+	// TODO rename field
+	// TODO fix comment
 	/**
 	 * definition du constructeur grille qui creer le tableau de jeu
 	 */
 	public Grille() 
 	{
-		/**
-		 * Construit une Grille Vide 
-		 */
 		
-		this.grille=new Case[NOMBRE_DE_lIGNES][NOMBRE_DE_COLONNES];
+		this.cases=new Case[NOMBRE_DE_lIGNES][NOMBRE_DE_COLONNES];
 		for(int j= 0 ; j < NOMBRE_DE_COLONNES;j++)
 		{
 			this.sommet[j]=0;
 			for (int i = 0; i<NOMBRE_DE_lIGNES;i++)
 			{
-				this.grille[i][j]=null;
+				this.cases[i][j]=null;
 								
 			}
 			
@@ -50,6 +57,8 @@ public class Grille
 		
 	
 	}
+	// TODO rename method
+	// TODO fix comment
 	/** 
 	 * Obtenir le nombre de pion dans une colonne
 	 * @param numcolonne choisis un numéro de colonne
--- src/Puissance4Game.java
+++ src/Puissance4Game.java
@@ -1,8 +1,16 @@
-
+// TODO finish writing comment
+/**
+ * classe qui cree
+ * @author guezelc
+ * 
+ */
 public class Puissance4Game
 {
+	// TODO write comment
+	// TODO rename field
 	private Grille grille;
 
+	// TODO fix comment
 	/**
 	 * Créé une partie de puissance4
 	 **/
@@ -11,9 +19,13 @@ public class Puissance4Game
 			
 	}
 
+		// TODO fix comment
+	/**
+	 * creation d'une nouvelle grille de jeu
+	 */
 	public void play() 
 	{
-		grille = new Grille();
+		this.grille = new Grille();
 		
 	}
 
