d46d7f13dd7ae6589667b4c21f1fddb7fe1dca39
GUEZEL Clement <guezelc@PC-B123-02.iut-valence.fr>
2015-05-27 10:56:51 +0200

    Creation of Button on the graphic

--- src/fr/iutvalence/info/m2103/project/p4/WindowPower4.java
+++ src/fr/iutvalence/info/m2103/project/p4/WindowPower4.java
@@ -1,13 +1,28 @@
 package fr.iutvalence.info.m2103.project.p4;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Color;
 import java.awt.Graphics;
 
-public class WindowPower4 implements Runnable 
+public class WindowPower4 extends JFrame implements Runnable 
 {
+	
+	private JButton button1 = new JButton("Col1");
+	private JButton button2 = new JButton("Col2");
+	private JButton button3 = new JButton("Col3");
+	private JButton button4 = new JButton("Col4");
+	private JButton button5 = new JButton("Col5");
+	private JButton button6 = new JButton("Col6");
+	private JButton button7 = new JButton("Col7");
+	
 	public void run()
 	{
+	
 	// Création du composant graphique associé à la fenêtre
 	// (à ce stade rien n'est affiché)
 	JFrame fenetre = new JFrame();
@@ -16,15 +31,30 @@ public class WindowPower4 implements Runnable
 	// Titre, dimensions (en pixel)
 	fenetre.setTitle("Power 4 ");
 	
-	fenetre.setSize(Grid.NUMBER_OF_COLUMNS*100+100, Grid.NUMBER_OF_LINES*100+100);
+	fenetre.setSize(Grid.NUMBER_OF_COLUMNS*100, Grid.NUMBER_OF_LINES*100);
 	
 	// Affichage de la fenêtre
 	fenetre.setVisible(true);
 	fenetre.setContentPane(new Panneau());
+	fenetre.setBackground(Color.BLACK);
 	
 	
 	
+	button1.setPreferredSize(new Dimension(65, 30));
+	button2.setPreferredSize(new Dimension(65, 30));
+	button3.setPreferredSize(new Dimension(65, 30));
+	button4.setPreferredSize(new Dimension(65, 30));
+	button5.setPreferredSize(new Dimension(65, 30));
+	button6.setPreferredSize(new Dimension(65, 30));
+	button7.setPreferredSize(new Dimension(65, 30));
 	
+	fenetre.add(button1);
+	fenetre.add(button2);
+	fenetre.add(button3);
+	fenetre.add(button4);
+	fenetre.add(button5);
+	fenetre.add(button6);
+	fenetre.add(button7);
 	
 	}
 }
